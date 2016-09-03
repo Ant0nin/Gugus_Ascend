14e0ccf0c9d8002fb51066374056db5ac8e8f823
Antonin Bernardin <antonin.bernardin@gmail.com>
2016-09-04 01:25:00 +0200

    event manager + sound manager

---
+++ Assets/Script/SoundManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioSource[] music_ambiant;
+    
+    public AudioSource effect_jump;
+
+    int currentMusicIndex = 0;
+
+	void OnEnable()
+    {
+        EventManager.StartListening("jump", PlayJumpSound);
+        EventManager.StartListening("change", ChangeAmbiantMusic);
+        music_ambiant[currentMusicIndex].Play();
+    }
+
+    void PlayJumpSound()
+    {
+        effect_jump.Play();
+    }
+
+    void ChangeAmbiantMusic()
+    {
+        music_ambiant[currentMusicIndex].Pause();
+
+        currentMusicIndex++;
+        if (currentMusicIndex >= music_ambiant.Length)
+            currentMusicIndex = 0;
+
+        music_ambiant[currentMusicIndex].Play();
+    }
+}
---
+++ Assets/Script/TriggerAutoChange.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerAutoChange : MonoBehaviour {
+
+    public float delay = 5f;
+
+    void Start()
+    {
+        StartCoroutine("DoCheck");
+    }
+
+    IEnumerator DoCheck()
+    {
+        for (;;)
+        {
+            EventManager.TriggerEvent("change");
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}

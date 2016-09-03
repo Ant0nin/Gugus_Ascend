ff0c6eddcaf0d92c2793bc46fc5cfcc60f970c69
Antonin Bernardin <antonin.bernardin@gmail.com>
2016-09-03 20:51:28 +0200

    commit missing files

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -35,10 +35,10 @@ public class PlayerController : MonoBehaviour {
         }
 
         /*Vector2 startPoint = transform.position;
-        Vector2 endPoint = new Vector2(transform.position.x, transform.position.y + 0.1f);
+        Vector2 endPoint = new Vector2(transform.position.x, transform.position.y + 0.1f);*/
 
-        Debug.DrawRay(new Vector3(startPoint.x, startPoint.y, 0), new Vector3(startPoint.x - endPoint.x, startPoint.y - endPoint.y, 0));*/
         isGrounded = Physics2D.Linecast(transform.position, tagGround.position, playerMask);
+        Debug.DrawRay(transform.position, tagGround.position);
 
         Debug.Log(isGrounded);
         

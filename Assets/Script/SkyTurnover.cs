175e29342935001d1b81b19a6fb3e2402b9f2e0e
Antonin Bernardin <antonin.bernardin@gmail.com>
2016-09-04 09:00:43 +0200

    arrow shooter

---
+++ Assets/Script/SkyTurnover.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyTurnover : MonoBehaviour {
+
+    public Texture2D[] skies;
+
+    int skyIndex = 0;
+    Shader myShader;
+    Material myMaterial;
+
+    void OnStart()
+    {
+        myMaterial = GetComponent<Material>();
+    }
+
+	void OnEnable()
+    {
+        EventManager.StartListening("change", ChangeSkyTexture);
+    }
+	
+	void ChangeSkyTexture()
+    {
+        skyIndex++;
+        if (skyIndex >= skies.Length)
+            skyIndex = 0;
+
+        //myMaterial.SetTexture()
+	}
+}

21c7d875f157c02c5d87f2df8da477fd5f58f40a
Antonin Bernardin <antonin.bernardin@gmail.com>
2016-09-05 20:41:06 +0200

    game exit on escape key

---
+++ Assets/Script/ExitGameOnInputEscape.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitGameOnInputEscape : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetButtonDown("escape"))
+        {
+            Application.Quit();
+        }
+	}
+}
